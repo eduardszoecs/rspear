619730b9f41742fe4fb1a46aa09a6a41c176f6e9
EDiLD <szoe8822@uni-landau.de>
2012-11-04 15:34:59 +0100

    always test for exact matches

--- R/match_traits.R
+++ R/match_traits.R
@@ -4,12 +4,11 @@
 #' @param y trait-table
 #' @param takex character string: taxa column-name
 #' @param takey character string: trait-table taxa column-name
-#' @param exact.first logic: should exact matches computed first?
 #' 
 #' @return a lookuptable, with original and matched data, as well as a match-value
 #' @export
 #' 
-match_traits <- function(x, y, takex, takey, exact.first = TRUE) {
+match_traits <- function(x, y, takex, takey) {
   ## empty object to fill with matches...
   merged <- NULL
   # which colums should be compared?
@@ -22,17 +21,15 @@ match_traits <- function(x, y, takex, takey, exact.first = TRUE) {
   x.id <- 1:length(xtake)
   y.id <- 1:length(ytake)
   ## match exactly first
-  if (exact.first) {
-    tmp <- match(xtakel, ytakel)
-    if (length(tmp) > 0) {
-      ## save in merged what matches 
-      merged <- na.omit(rbind(merged,
-                              data.frame(x.id = x.id,
-                                         y.id = y.id[tmp],
-                                         threshold = -1)))
-      ##save the unmatched obs indexes in x.id 
-      x.id <- x.id[!(x.id %in% merged[ , 1])]
-    }
+  tmp <- match(xtakel, ytakel)
+  if (length(tmp) > 0) {
+    ## save in merged what matches 
+    merged <- na.omit(rbind(merged,
+                            data.frame(x.id = x.id,
+                                       y.id = y.id[tmp],
+                                       threshold = -1)))
+    ##save the unmatched obs indexes in x.id 
+    x.id <- x.id[!(x.id %in% merged[ , 1])]
   }
   ## match approximately for each threshold in s
   for (i in seq(0, 0.5, 0.1)) {
