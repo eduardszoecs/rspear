158a2d5e9cec1319212ae1f852c7b7387be6d287
EDiLD <szoe8822@uni-landau.de>
2012-12-26 03:07:17 +0100

    style

--- R/spear.R
+++ R/spear.R
@@ -120,7 +120,7 @@ spear <- function(x, taxa = NULL, abundance = NULL,  group = NULL,
     trait$SPEAR[is.na(trait$SPEAR)] <- 0
   }
   df <- merge(x, trait, by.x = taxa, by.y = "taxa_data")
-  spear <- ddply(df, group, function(x) c(SPEAR = 100 * sum(log(x[ ,abundance] + 1) * x$SPEAR) / sum(log(x[ ,abundance] + 1))))
+  spear <- ddply(df, group, function(x) c(SPEAR = 100 * sum(log(x[ , abundance] + 1) * x$SPEAR) / sum(log(x[ , abundance] + 1))))
   out = list(spear = spear, 
              traits = trait[order(trait$match_val, decreasing = TRUE, na.last = FALSE), c("taxa_data", "taxa_matched", "match_val", "region", "exposed", "generationTime", "sensitivity", "migration", "SPEAR")])
   class(out$traits) <- c("data.frame", "spear")
